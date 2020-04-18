6206f975f85038f6d49be3070bb7a2e117846de9
PurestEarth <jaremaradom@gmail.com>
2020-04-18 18:00:09 +0200

    Properly working location sensor

--- app/src/main/java/com/example/lab4sensors/ui/GPS/DashboardFragment.kt
+++ app/src/main/java/com/example/lab4sensors/ui/GPS/DashboardFragment.kt
@@ -1,5 +1,8 @@
 package com.example.lab4sensors.ui.GPS
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
@@ -7,12 +10,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.lab4sensors.R
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import java.util.*
 
 
 class DashboardFragment : Fragment() {
@@ -33,22 +39,77 @@ class DashboardFragment : Fragment() {
         txtLocation = root.findViewById(R.id.text_dashboard)
         mFusedLocationClient =
             activity?.let { LocationServices.getFusedLocationProviderClient(it) }
-        mFusedLocationClient?.lastLocation?.addOnSuccessListener {
-                location: Location? -> Log.i("BENIZ", location.toString()) }
-                //location: Location? -> txtLocation?.text = location.toString() }
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
 
         })
+        activity?.let { settlePermissions(it) }
         return root
     }
 
     override fun onPause(){
         super.onPause()
-        //todo
+        if (mFusedLocationClient != null) {
+            // mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
     }
 
     override fun onResume(){
         super.onResume()
         //todo
     }
+
+    private fun settlePermissions(activity: Activity) {
+        return if (context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) } != PackageManager.PERMISSION_GRANTED
+            && context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.ACCESS_COARSE_LOCATION) } != PackageManager.PERMISSION_GRANTED) {
+            Log.i("BENIZ", "Dalej nie ma")
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                1000)
+        } else if(context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) } == PackageManager.PERMISSION_GRANTED
+                    && context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.ACCESS_COARSE_LOCATION) } == PackageManager.PERMISSION_GRANTED) {
+            handleLocationDisplay()
+        }
+        else {
+            Toast.makeText(context, "Uzurpator", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.i("BENIZ", "on request permission result")
+        if(requestCode == 1000 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            handleLocationDisplay()
+        } else {
+            Log.i("BENIZ", "Failure")
+            Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun handleLocationDisplay(){
+        mFusedLocationClient?.lastLocation?.addOnSuccessListener {
+                location: Location? ->
+            run {
+                Log.i("BENIZ", location.toString())
+                if(location !== null){
+                    val wayLatitude = location.latitude
+                    val wayLongitude = location.longitude
+                    txtLocation?.text = (String.format(Locale.US, "%s -- %s", wayLatitude, wayLongitude))
+                } else {
+                    Toast.makeText(context, "Make sure location is enabled", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        mFusedLocationClient?.lastLocation?.addOnFailureListener{
+            Toast.makeText(context, "Make sure location is enabled", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
